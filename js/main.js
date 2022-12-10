7a5ed6386ecb95f6c96633c68b509e8259bb5ad5
Sakil Anwar <sakilanwar60@gmail.com>
2022-12-10 23:39:29 +0600

    hero area done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
+
 window.addEventListener('scroll', function () {
     const header = document.querySelector('.header-area .menu-area');
-   // const bannarArea = this.document.querySelector('.bannar-area')
+    const mobileMenuBtn = this.document.querySelector('.mobile-menu-btn');
+    const languages = this.document.querySelector('.languages');
     header.classList.toggle("sticky", window.scrollY > 100);
-    // bannarArea.classList.toggle('bannarSpacing', this.window.scrollY > 100)
+    mobileMenuBtn.classList.toggle("sticky-btn", window.scrollY > 100);
+    languages.classList.toggle("sticky-flag", window.scrollY > 100);
+
+})
+
+
+const mobileMenuBtn = document.querySelector('.mobile-menu-btn button');
+const mobileMenu = document.querySelector(".mobile-nav")
+mobileMenuBtn.addEventListener('click', function () {
+    mobileMenu.classList.toggle('active');
 })
