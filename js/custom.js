397ebe06d7274fccdae5a09bbc043e4ac89e7337
Sakil Anwar <sakilanwar60@gmail.com>
2022-12-13 01:15:59 +0600

    small changes

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,3 @@
 jQuery(".mobile-nav-wrapper ul").on('click', 'li', function () {
-    jQuery(this).toggleClass('active').siblings().removeClass('active');
+    jQuery(this).addClass('active').siblings().removeClass('active');
 });
